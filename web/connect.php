16782560b79e3bc981389cd1d757f59c4296928b
Andrei <lungeanuandreivirgil@gmail.com>
2016-04-08 19:39:14 +0300

    mysql config

--- web/connect.php
+++ web/connect.php
@@ -1,6 +1,6 @@
 <?php
 //Open a new connection to the MySQL server
-$mysqli = new mysqli('ivgz2rnl5rh7sphb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com','pv9trg9ydxga6ay3','ydyptuiyurxa3ww1','dugt8tdb574mfzbj');
+$mysqli = new mysqli('ivgz2rnl5rh7sphb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com','dugt8tdb574mfzbj','ydyptuiyurxa3ww1','pv9trg9ydxga6ay3');
 //Output any connection error
 if ($mysqli->connect_error) {
     die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
