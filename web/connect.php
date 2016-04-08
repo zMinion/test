723e10a547e5d311e51ccacffb25bde706445236
Andrei <lungeanuandreivirgil@gmail.com>
2016-04-08 19:36:15 +0300

    new mysql host

--- web/connect.php
+++ web/connect.php
@@ -1,6 +1,6 @@
 <?php
 //Open a new connection to the MySQL server
-$mysqli = new mysqli('fdb3.awardspace.net','2037932_db','Parola1234','2037932_db');
+$mysqli = new mysqli('ivgz2rnl5rh7sphb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com','pv9trg9ydxga6ay3','ydyptuiyurxa3ww1','dugt8tdb574mfzbj');
 //Output any connection error
 if ($mysqli->connect_error) {
     die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
