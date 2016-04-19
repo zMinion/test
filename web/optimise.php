d98aafd2743824c9da789226cd09892b4d511c54
zMinion <pentru.orice.prostie@gmail.com>
2016-04-19 23:35:41 +0300

    Add optimise

---
+++ web/optimise.php
@@ -0,0 +1,45 @@
+<?php
+
+include 'connect.php';
+
+function JPGoptimiser($JPGfile, &$error = '')
+{
+   $ch = curl_init('http://jpgoptimiser.com/optimise');
+   curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+   curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+   curl_setopt($ch, CURLOPT_POST, 1);
+   curl_setopt($ch, CURLOPT_POSTFIELDS, array('input' => '@'.$JPGfile));
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+   $jpg = curl_exec($ch);
+   $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+   if ($status !== 200) {
+      $error = 'jpgoptimiser.com request failed: HTTP code ' . $status;
+      return false;
+   }
+   $curl_error = curl_error($ch);
+   if (!empty($curl_error)) {
+      $error = 'jpgoptimiser.com request failed: CURL error ' . $curl_error;
+      return false;
+   }
+   curl_close($ch);
+   return $jpg;
+}
+
+$file = $_FILES['optimise']['tmp_name'];
+$result = JPGoptimiser($file, $error);
+if (false === $result) { die("{$error}\n"); }
+
+header("Content-type: image/jpeg");
+ // NOTE: Possible header injection via $basename
+header("Content-Disposition: attachment; filename=" . $_FILES['optimise']['name']);
+header('Content-Transfer-Encoding: binary');
+header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+ 
+// Force download of image file 
+// $size = filesize($image);
+// header("Content-Length: " . $size);
+fpassthru($result);
+// Clear memory
+ exit();
+ 
+ ?>
