9de3f39bcea0045683da3b66d8f9f0c733a99b4e
zMinion <pentru.orice.prostie@gmail.com>
2016-04-27 10:07:40 +0300

    Errors

---
+++ web/error.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+    <title>DealFactory - Errors</title>
+    <link rel="shortcut icon" href="/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/theme-bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/style-simple.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="row">
+            <div class="col-md-5 col-md-offset-2">
+                <div class="well-lg">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Error</h3>
+                        </div>
+                        <div class="panel-body">
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <div id="logo-errors" class="center-block" style="display:none"></div>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="input-group-lg">
+x x x
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <br>
+                                    <div class="row">
+                                        
+                                        <div class="col-md-8 text-center col-md-offset-2">
+                                            <div class="panel panel-default text-center">
+                                                <div class="panel-heading">head</div>
+                                                <div class="panel-body">
+                                                    body
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+</body></html>
+
