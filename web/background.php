a21572e1d15942d406b87fa71f6b704cd0fa27c7
Andrei <lungeanuandreivirgil@gmail.com>
2016-05-01 17:36:49 +0300

    Beautifier 2

--- web/background.php
+++ web/background.php
@@ -1,11 +1,9 @@
 <?php
-
-	$randseed=rand(0,16);
-	$str=file_get_contents('http://www.bing.com/HPImageArchive.aspx?format=js&idx='.$randseed.'&n=1&mkt=en-US');
-	$array = json_decode($str);
-	$imgurl = 'http://www.bing.com'.$array->{"images"}[0]->{"urlbase"}.'_1920x1080.jpg';
-	
-	if($imgurl){  
-        header('Location: '.$imgurl);
-        die();
-	}
+$randseed = rand(0, 16);
+$str      = file_get_contents('http://www.bing.com/HPImageArchive.aspx?format=js&idx=' . $randseed . '&n=1&mkt=en-US');
+$array    = json_decode($str);
+$imgurl   = 'http://www.bing.com' . $array->{"images"}[0]->{"urlbase"} . '_1920x1080.jpg';
+if ($imgurl) {
+	header('Location: ' . $imgurl);
+	die();
+}
